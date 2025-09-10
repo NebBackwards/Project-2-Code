db9f66be908ae1b69f78718ef85f229d07838c75
NebBackwards <benproctor2022@gmail.com>
2025-09-10 15:27:36 -0400

    point collision check

--- CollisionChecking.cpp
+++ CollisionChecking.cpp
@@ -9,7 +9,17 @@
 
 bool isValidPoint(double x, double y, const std::vector<Rectangle>& obstacles)
 {
-    //Fillout 
+    bool isValid = true;
+    for (Rectangle ob : obstacles){
+        double max_x = ob.x + ob.width;
+        double max_y = ob.y + ob.height;
+        if ((x <= max_x) && (x >= ob.x)){
+            if ((y <= max_y) && (y >= ob.y)){
+                isValid = false;
+            }
+        }
+    }
+    return isValid;
 }
 
 bool isValidSquare(double x, double y, double theta, double sideLength, const std::vector<Rectangle>& obstacles,double hbound, double lbound)
--- CollisionChecking.h
+++ CollisionChecking.h
@@ -31,12 +31,7 @@ struct Robot
 
 // Intersect the point (x,y) with the set of rectangles. If the point lies
 // outside of all obstacles, return true.
-bool isValidPoint(double x, double y, const std::vector<Rectangle>& obstacles){
-
-    for (int i = 0; i < obstacles.size(); i++){
-        obstacles[i]
-    }
-}
+bool isValidPoint(double x, double y, const std::vector<Rectangle>& obstacles);
 
 // Intersect a square with center at (x,y), orientation theta, and the given
 // side length with the set of rectangles. If the square lies outside of all
